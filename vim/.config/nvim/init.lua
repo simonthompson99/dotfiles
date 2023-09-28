0af2b46c541d96cafcc814db65ec9f045b37929b
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2023-09-28 10:30:25 +0100

    small updates

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -20,3 +20,6 @@ vim.opt.rtp:prepend(lazypath)
 require "plugins"
 
 dofile(vim.g.base46_cache .. "defaults")
+
+vim.cmd "source ~/.config/nvim/lua/custom/ftplugins/json.vim"
+vim.cmd "source ~/.config/nvim/lua/custom/ftplugins/python.vim"
